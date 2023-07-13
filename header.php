048b501f49120e5b632b1350b0835803c07dea94
Gloria <gloriadamase@gmail.com>
2023-07-13 16:46:08 +0200

    last

--- header.php
+++ header.php
@@ -49,9 +49,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
+                <?php wp_nav_menu(array('container_class' => 'menu-header', 'theme_location' => 'primary')) ?>
             </ul>
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -60,7 +58,7 @@
             </div>
         </div>
         </nav>
-        
+
 </header>
 
 <main class="container my-5">
